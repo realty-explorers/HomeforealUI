828b6134a6efaf981ac4145a1317dd09dabfe88c
SharonFabin <i.sharon.fabin@gmail.com>
2023-02-01 15:38:22 +0200

    final nginx configs

--- security/setup-certificates.sh
+++ security/setup-certificates.sh
@@ -1,2 +1,1 @@
-docker compose exec webserver nginx -s reload
-docker-compose run --rm  certbot certonly --webroot --webroot-path /var/www/certbot/ -d homeforeal.com
+docker-compose run --rm  certbot certonly --webroot --webroot-path /var/www/certbot/ -d example.com
