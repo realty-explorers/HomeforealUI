baf608ab7c8499d442e36d3e7e77b6ec90dc6202
Sharon Fabin <i.sharon.fabin@gmail.com>
2023-07-26 12:42:58 +0300

    Property details section

--- ui/src/content/Dashboards/Analytics/PropertyHeader/PropertyDetails.tsx
+++ ui/src/content/Dashboards/Analytics/PropertyHeader/PropertyDetails.tsx
@@ -1,5 +1,6 @@
 import Property from '@/models/property';
-import { Grid } from '@mui/material';
+import { Grid, Typography } from '@mui/material';
+import styles from './PropertyHeaderStyles.module.scss';
 
 type PropertyDetailsProps = {
   property: Property;
@@ -8,8 +9,15 @@ const PropertyDetails = (props: PropertyDetailsProps) => {
   return (
     <>
       <Grid container>
-        <Grid item xs={8}></Grid>
-        <Grid item xs={4}></Grid>
+        <Typography className={styles.detailsText}>
+          Just off the Bellville square you will find this historic 1880’s
+          3-story Victorian Eastlake home sitting on a one-acre corner lot. This
+          home has original wood trim, long leaf pine floors, transoms, 12’
+          ceilings, stained glass windows, claw foot bathtubs, milk glass light
+          fixtures and more. MAIN HOUSE: The 3398 square feet includes the 1st
+          and 2nd floors. The 3rd floor has an additional 926 SF (not included
+          in total) that had been used for bedrooms and a game room.
+        </Typography>
       </Grid>
     </>
   );
