8c84ffa22e9e5b92c94cd705f83f1a363bf6ca50
Sharon Fabin <i.sharon.fabin@gmail.com>
2023-10-19 17:22:44 +0300

    Remove mobile layout

--- ui/src/content/Overview/Hero/index.tsx
+++ ui/src/content/Overview/Hero/index.tsx
@@ -12,7 +12,7 @@ import Image from "next/image";
 function Hero() {
   return (
     <div className="flex flex-col items-center h-full w-full">
-      <div className="hidden lg:flex flex-col justify-center items-center">
+      <div className="lg:flex flex-col justify-center items-center">
         <HeroSection />
         <SearchSection />
         <HowItWorksSection />
@@ -21,41 +21,41 @@ function Hero() {
         <ContactUsComponent />
         <Footer />
       </div>
-      <div className="flex lg:hidden justify-center items-center w-full h-full relative">
-        <div className="absolute bottom-0 h-[70%] w-full">
-          <Image
-            src="/static/images/placeholders/covers/house_bg.jpg"
-            alt="house"
-            fill
-            className="object-cover object-center w-full h-full"
-          />
-        </div>
-
-        <div className="flex flex-wrap w-[40%] z-[1]">
-          <div className="flex w-full justify-between items-center gap-2">
-            <Typography className="">
-              find untapped
-            </Typography>
-            <img
-              className="w-12 h-12"
-              src="/static/images/placeholders/illustrations/ball.png"
-            />
-          </div>
-          <div className="flex w-full">
-            <Typography className="">
-              Investments
-            </Typography>
-          </div>
-
-          <div className="flex mt-8">
-            <Typography className="">
-              Home for real is a platform that helps leading real estate agents
-              find, match and analyze real estate. unlock hidden real estate
-              opportunists
-            </Typography>
-          </div>
-        </div>
-      </div>
+      {/* <div className="flex lg:hidden justify-center items-center w-full h-full relative"> */}
+      {/*   <div className="absolute bottom-0 h-[70%] w-full"> */}
+      {/*     <Image */}
+      {/*       src="/static/images/placeholders/covers/house_bg.jpg" */}
+      {/*       alt="house" */}
+      {/*       fill */}
+      {/*       className="object-cover object-center w-full h-full" */}
+      {/*     /> */}
+      {/*   </div> */}
+      {/**/}
+      {/*   <div className="flex flex-wrap w-[40%] z-[1]"> */}
+      {/*     <div className="flex w-full justify-between items-center gap-2"> */}
+      {/*       <Typography className=""> */}
+      {/*         find untapped */}
+      {/*       </Typography> */}
+      {/*       <img */}
+      {/*         className="w-12 h-12" */}
+      {/*         src="/static/images/placeholders/illustrations/ball.png" */}
+      {/*       /> */}
+      {/*     </div> */}
+      {/*     <div className="flex w-full"> */}
+      {/*       <Typography className=""> */}
+      {/*         Investments */}
+      {/*       </Typography> */}
+      {/*     </div> */}
+      {/**/}
+      {/*     <div className="flex mt-8"> */}
+      {/*       <Typography className=""> */}
+      {/*         Home for real is a platform that helps leading real estate agents */}
+      {/*         find, match and analyze real estate. unlock hidden real estate */}
+      {/*         opportunists */}
+      {/*       </Typography> */}
+      {/*     </div> */}
+      {/*   </div> */}
+      {/* </div> */}
     </div>
   );
 }
