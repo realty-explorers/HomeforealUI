c0f03abdcbc03287a9eeb5dcb4541cd993e14f12
Sharon Fabin <i.sharon.fabin@gmail.com>
2025-03-23 00:33:14 +0200

    feat: deny offers of unverified users

--- src/content/Dashboards/Analytics/Offer/MakeOfferButton.tsx
+++ src/content/Dashboards/Analytics/Offer/MakeOfferButton.tsx
@@ -1,6 +1,9 @@
+import { useAppDispatch } from '@/store/hooks';
+import { setShowVerificationDialog } from '@/store/slices/authSlice';
 import RequestQuoteIcon from '@mui/icons-material/RequestQuote';
 import { Button, Typography } from '@mui/material';
 import clsx from 'clsx';
+import { useSession } from 'next-auth/react';
 import { useSearchParams } from 'next/navigation';
 
 type MakeOfferButtonProps = {
@@ -9,9 +12,19 @@ type MakeOfferButtonProps = {
 const MakeOfferButton = (props: MakeOfferButtonProps) => {
   const searchParams = useSearchParams();
   const referral = searchParams.get('referral');
+  const { data: session } = useSession();
+  const dispatch = useAppDispatch();
+
+  const hanldeOfferClick = () => {
+    if (!session?.user?.verified) {
+      dispatch(setShowVerificationDialog(true));
+    } else {
+      props.onClick();
+    }
+  };
   return (
     <Button
-      onClick={props.onClick}
+      onClick={hanldeOfferClick}
       className={clsx([
         'p-[3px] rounded-full bg-gradient-to-r from-indigo-500 to-pink-500 group absolute bottom-2 left-3 z-10 animate-fadeDelayed opacity-0 ',
         referral === 'projo' && 'w-[calc(100%-1rem)] xs:w-auto'
