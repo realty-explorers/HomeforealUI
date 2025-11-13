72139ecaf4f54783d94d2e724bc1ad42bfb41536
aradpi9 <aradpi9@gmail.com>
2025-11-13 15:17:51 +0200

    Update findDeals.ts

--- pages/api/properties/findDeals.ts
+++ pages/api/properties/findDeals.ts
@@ -2,9 +2,6 @@ import type { NextApiRequest, NextApiResponse } from 'next/'
 import { findDeals } from "@/api/deals_api";
 import { AxiosError } from 'axios';
 
-export const config = {
-  runtime: 'edge'
-};
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
     try {
         if (req.method !== 'POST') throw Error('Invalid request method');
