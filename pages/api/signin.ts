188bfbf758a8e6229064bd4d96bae9c105edab12
aradpi9 <aradpi9@gmail.com>
2025-11-13 12:11:19 +0200

    Update signin.ts

--- pages/api/signin.ts
+++ pages/api/signin.ts
@@ -1,4 +1,6 @@
-export const runtime = 'edge';
+export const config = {
+  runtime: 'edge'
+};
 
 export default async function handler(req, res) {
   const callbackUrl = (req.query.callbackUrl as string) || '';
