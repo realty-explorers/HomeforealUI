11d08667e9db9ce6d49b0b0bccd65730ac90749a
aradpi9 <aradpi9@gmail.com>
2025-11-13 12:12:30 +0200

    Update API config to specify edge runtime

--- pages/api/properties/findProperties.ts
+++ pages/api/properties/findProperties.ts
@@ -2,7 +2,9 @@ import type { NextApiRequest, NextApiResponse } from 'next/'
 import { findProperties } from "@/api/deals_api";
 import { AxiosError } from 'axios';
 
-export const runtime = 'edge';
+export const config = {
+  runtime: 'edge'
+};
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
     try {
